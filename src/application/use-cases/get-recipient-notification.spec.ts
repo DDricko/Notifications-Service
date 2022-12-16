3202bf1b3699244ac650ff4abdde5deccc1a3b1b
Rodrigo Nascimento <rodrigowaltmann.n@gmail.com>
2022-12-16 11:24:29 -0300

    Criação dos testes do caso de uso getRcipientNotification

---
+++ src/application/use-cases/get-recipient-notification.spec.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { InMemoryNotificationRepository } from "@test/repositories/in-memory-notifications-repository";
+import { makeNotification } from "@test/factories/notification-factory";
+import { GetRecipientNotification } from "./get-recipient-notification";
+
+describe('Get recipient notifications', () => {
+    it('should be able to get recipient notifications', async () => {
+        const notificationRepository = new InMemoryNotificationRepository();
+        const getRecipientNotification = new GetRecipientNotification(notificationRepository);
+
+        await notificationRepository.create(makeNotification({ recipientId: 'recipient-1'}));
+
+        await notificationRepository.create(makeNotification({ recipientId: 'recipient-1'}));
+
+        await notificationRepository.create(makeNotification({ recipientId: 'recipient-2'}));
+
+        const { notifications } = await getRecipientNotification.execute({ recipientId: 'recipient-1' });
+
+        expect(notifications).toHaveLength(2);
+        expect(notifications).toEqual(expect.arrayContaining([
+            expect.objectContaining({ recipientId: 'recipient-1' }),
+            expect.objectContaining({ recipientId: 'recipient-1' }),
+
+        ]))
+    });
+});
