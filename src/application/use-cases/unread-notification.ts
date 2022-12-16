d52b55608465cb140d3f51da8bcc36eee202b028
Rodrigo Nascimento <rodrigowaltmann.n@gmail.com>
2022-12-16 11:51:24 -0300

    Criação do caso de uso unreadNotification e testes

---
+++ src/application/use-cases/unread-notification.ts
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from "@nestjs/common";
+import { NotificationRepository } from "../repositories/notification-repository";
+import { NotificationNotFound } from "./errors/notifications-not-found";
+
+interface UnreadNotificationRequest {
+    notificationId: string; 
+}
+
+type UnreadNotificationResponse = void;
+
+
+@Injectable()//decorator
+export class UnreadNotification {
+    constructor(private notificationRepository: NotificationRepository) {}
+
+    async execute(
+        request: UnreadNotificationRequest,
+    ): Promise<UnreadNotificationResponse> {
+        const { notificationId } = request;
+
+        const notification = await this.notificationRepository.findById(notificationId);
+
+        if (!notification) {
+            throw new NotificationNotFound();
+        }
+
+        notification.unread();
+
+        await this.notificationRepository.save(notification);
+    }
+}
